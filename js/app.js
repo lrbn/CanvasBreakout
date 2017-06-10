c0695246d7cc64debae85cc4a42fb4ac98eb765b
brandon <lrbn86@gmail.com>
2017-06-10 19:21:30 -0400

    Added paddle

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ var y = canvas.height - 30;
 var ballRadius = 10;
 var dx = 2;
 var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall(fillStyle) {
   ctx.beginPath();
@@ -15,9 +20,18 @@ function drawBall(fillStyle) {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     var randomColor = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
     ctx.fillStyle = colors[randomColor];
@@ -28,7 +42,31 @@ function draw() {
     ctx.fillStyle = colors[randomColor];
     dy = -dy;
   }
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
   x += dx;
   y += dy;
 }
-setInterval(draw, 1);
+
+function keyDownHandler(ev) {
+  if (ev.keyCode == 39) {
+    rightPressed = true;
+  } else if (ev.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(ev) {
+  if (ev.keyCode == 39) {
+    rightPressed = false;
+  } else if (ev.keyCode == 37) {
+    leftPressed = false;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+setInterval(draw, 10);
